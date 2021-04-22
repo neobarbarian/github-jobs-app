d6fb60ad73f2f6cd64495e7614c8cfc099227add
Erik Hanson <neobarbarian@Eriks-MacBook-Pro.local>
2021-04-21 19:44:37 -0500

    main job list panel frame and basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'github-jobs-app';
+  faMoon = faMoon;
+  faSun = faSun;
 }
